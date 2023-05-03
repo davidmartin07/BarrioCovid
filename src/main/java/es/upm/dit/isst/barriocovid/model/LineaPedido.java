0cd059cb91775be0d6d091196ac77f85f518ecfe
jorgemolinal <david.martin.levy@alumnos.upm.es>
2023-05-03 22:17:14 +0200

    first commit

---
+++ src/main/java/es/upm/dit/isst/barriocovid/model/LineaPedido.java
@@ -0,0 +1,154 @@
+package es.upm.dit.isst.barriocovid.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="lineaPedido")
+public class LineaPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 25, nullable = false)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @Column
+    private double cantidad;
+
+    @Column
+    private double precio;
+
+    @Column
+    private boolean processed;
+
+    @Column
+    private double total;
+
+    @ManyToOne
+    private Usuario usuario;
+    
+    @ManyToOne
+    private Producto producto;
+
+    public LineaPedido() {
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+
+    public LineaPedido(String nombre, String descripcion, double cantidad, double precio, double total) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "LineaPedido [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", cantidad="
+                + cantidad + ", precio=" + precio + ", total=" + total  + ", producto=" + producto
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LineaPedido other = (LineaPedido) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }  
+}
